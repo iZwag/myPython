0a5296b070ff097aaa46becc037f0ee7fed3a0d2
Jon Zwaig Kolstad <jon.zk@hotmail.com>
2019-02-21 00:29:46 +0100

    Add working convert_norwegian_symbols, but only for lower case

--- norenglish.py
+++ norenglish.py
@@ -1,28 +1,33 @@
 class NorEnglish:
-    
-    NOR_SYMBOLS = {'æ','Æ','ø','Ø','å','Å'}
-    NOR_DBL_SYMBOLS = { 'æ': "ae",
+    NOR_SYMBOLS =     { 'æ',
+                        'Æ',
+                        'ø',
+                        'Ø',
+                        'å',
+                        'Å'}
+    NOR_DBL_LOWER =   { 'æ': "ae",
                         'ø': "oe",
                         'å': "aa"}
+    NOR_DBL_CAPITAL = { 'Æ': "Ae",
+                        'Ø': "Oe",
+                        'Å': "Aa"}
+    NOR_DBL_CAPS =    { 'Æ': "AE",
+                        'Ø': "OE",
+                        'Å': "AA"}
     NOR_ALT_SYMBOLS = { 'æ': 'e',
                         'ø': 'o',
-                        'å': 'a'}    
+                        'å': 'a'}
 
     def __init__(self, string=""):
         self._string = string
 
     def convert_norwegian_symbols(self, string):
-        
-        self._string = string
-        
-        for char in self._string:
-            if char in self.NOR_SYMBOLS:
-                list(self._string).insert(char, 
-                                    self.NOR_DBL_SYMBOLS[char.lower()])
+        self._string = ""
+        string_list = list(string)
+        for i in range(len(string_list)):
+            if string_list[i] in self.NOR_SYMBOLS:
+                string_list[i] = self.NOR_DBL_SYMBOLS[string_list[i].lower()]
+            self._string += string_list[i]
 
         return self._string
 
-abc = NorEnglish()
-print(abc.convert_norwegian_symbols("Å øve på drømmen er ærlig ærend"))
-
-        
